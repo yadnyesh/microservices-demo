b1b276185ddafff69cd59972c27da29d12f851da
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-12-31 19:41:19 +0530

    Working code

---
+++ twitter-to-kafka-service/src/main/java/io/yadnyesh/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,30 @@
+package io.yadnyesh.microservices.demo.twitter.to.kafka.service;
+
+import io.yadnyesh.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@Slf4j
+@EnableConfigurationProperties
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("App started....");
+        log.info(twitterToKafkaServiceConfigData.getTwitterKeywords().toString());
+    }
+}
---
+++ twitter-to-kafka-service/src/test/java/io/yadnyesh/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package io.yadnyesh.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
