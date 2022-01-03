7559befa242566840c455b6a8e6ffc51255bcd78
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-03 16:27:11 +0530

    Enabling the configuration for KafkaAdmin

---
+++ kafka/kafka-admin/src/main/java/io/yadnyesh/microservices/demo/kafka/admin/KafkaAdminConfig.java
@@ -0,0 +1,28 @@
+package io.yadnyesh.microservices.demo.kafka.admin;
+
+import io.yadnyesh.microservices.demo.config.KafkaConfigData;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.Map;
+
+@Configuration
+@EnableRetry
+public class KafkaAdminConfig {
+
+    private final KafkaConfigData kafkaConfigData;
+
+    public KafkaAdminConfig(KafkaConfigData kafkaConfigData) {
+        this.kafkaConfigData = kafkaConfigData;
+    }
+
+    @Bean
+    public AdminClient adminClient() {
+        return AdminClient.create(Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaConfigData.getBootstrapServers()));
+    }
+
+}
