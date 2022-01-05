681fd0051cd0a7166de1e5562f32a8fcc8eaa410
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-05 21:49:59 +0530

    Added missing property

--- app-config-data/src/main/java/io.yadnyesh.microservices.demo.config/KafkaConfigData.java
+++ app-config-data/src/main/java/io.yadnyesh.microservices.demo.config/KafkaConfigData.java
@@ -13,6 +13,7 @@ public class KafkaConfigData {
     private String bootstrapServers;
     private String schemaRegistryUrlKey;
     private String schemaRegistryUrl;
+    private String topicName;
     private List<String> topicNamesToCreate;
     private Integer numOfPartitions;
     private Short replicationFactor;
