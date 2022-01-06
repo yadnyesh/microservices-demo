4c1997a9d94e31e853c0adc77e71df890b8fd66e
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-06 15:37:39 +0530

    RetryConfig bean still not being able to be identified and injected.

--- common-config/src/main/java/io/yadnyesh/micorservices/demo/common/config/RetryConfig.java
+++ common-config/src/main/java/io/yadnyesh/micorservices/demo/common/config/RetryConfig.java
@@ -10,23 +10,39 @@ import org.springframework.retry.support.RetryTemplate;
 @Configuration
 public class RetryConfig {
 
-    private final RetryConfigData retryConfigData;
+//    private final RetryConfigData retryConfigData;
+//
+//    public RetryConfig(RetryConfigData retryConfigData) {
+//        this.retryConfigData = retryConfigData;
+//    }
 
-    public RetryConfig(RetryConfigData retryConfigData) {
-        this.retryConfigData = retryConfigData;
-    }
+//    @Bean
+//    public RetryTemplate retryTemplate() {
+//        RetryTemplate retryTemplate = new RetryTemplate();
+//        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
+//        exponentialBackOffPolicy.setInitialInterval(retryConfigData.getInitialIntervalMs());
+//        exponentialBackOffPolicy.setMaxInterval(retryConfigData.getMaxIntervalMs());
+//        exponentialBackOffPolicy.setMultiplier(retryConfigData.getMultiplier());
+//        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
+//
+//        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+//        simpleRetryPolicy.setMaxAttempts(retryConfigData.getMaxAttempts());
+//        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+//
+//        return  retryTemplate;
+//    }
 
     @Bean
     public RetryTemplate retryTemplate() {
         RetryTemplate retryTemplate = new RetryTemplate();
         ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
-        exponentialBackOffPolicy.setInitialInterval(retryConfigData.getInitialIntervalMs());
-        exponentialBackOffPolicy.setMaxInterval(retryConfigData.getMaxIntervalMs());
-        exponentialBackOffPolicy.setMultiplier(retryConfigData.getMultiplier());
+        exponentialBackOffPolicy.setInitialInterval(1000);
+        exponentialBackOffPolicy.setMaxInterval(10000);
+        exponentialBackOffPolicy.setMultiplier(2.0);
         retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
 
         SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
-        simpleRetryPolicy.setMaxAttempts(retryConfigData.getMaxAttempts());
+        simpleRetryPolicy.setMaxAttempts(3);
         retryTemplate.setRetryPolicy(simpleRetryPolicy);
 
         return  retryTemplate;
